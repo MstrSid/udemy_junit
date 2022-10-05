38f683a2aa0787d5e7d0e98ca339cfa4bc13ce2a
Eugene Kos <mstrsid@live.com>
2022-10-05 12:26:19 +0300

    homework done

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,7 +1,22 @@
 package org.example;
 
 public class Calculator {
-    public int sum(int a, int b) {
+    public double plus(double a, double b) {
         return a + b;
+    }
+
+    public double minus(double a, double b) {
+        return a - b;
+    }
+
+    public double div(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else return 0;
+
+    }
+
+    public double mul(double a, double b) {
+        return a * b;
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -7,13 +7,50 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    void sum() {
-        int expected = 15;
-        int result = new Calculator().sum(10, 5);
-        assertEquals(expected, result);
+    void plus() {
+        double expected = 15;
+        double result = new Calculator().plus(10, 5);
+        assertEquals(expected, result, 0.001);
 
-        int expected2 = 150;
-        int result2 = new Calculator().sum(100, 50);
-        assertEquals(expected2, result2);
+        double expected2 = 150;
+        double result2 = new Calculator().plus(100, 50);
+        assertEquals(expected2, result2, 0.001);
+    }
+
+    @Test
+    void minus() {
+        double expected = 15;
+        double result = new Calculator().minus(25, 10);
+        assertEquals(expected, result, 0.001);
+
+        double expected2 = 150;
+        double result2 = new Calculator().minus(200, 50);
+        assertEquals(expected2, result2, 0.001);
+    }
+
+    @Test
+    void div() {
+        double expected = 20;
+        double result = new Calculator().div(40, 2);
+        assertEquals(expected, result, 0.001);
+
+        double expected2 = 7;
+        double result2 = new Calculator().div(14, 2);
+        assertEquals(expected2, result2, 0.001);
+
+        double expected3 = 0;
+        double result3 = new Calculator().div(14, 0);
+        assertEquals(expected3, result3, 0.001);
+    }
+
+    @Test
+    void mul() {
+        double expected = 30;
+        double result = new Calculator().mul(2, 15);
+        assertEquals(expected, result, 0.001);
+
+        double expected2 = 150;
+        double result2 = new Calculator().mul(75, 2);
+        assertEquals(expected2, result2, 0.001);
     }
 }
